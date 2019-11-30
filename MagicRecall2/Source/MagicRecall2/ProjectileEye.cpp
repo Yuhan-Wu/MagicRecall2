f75c97abb6f87680df5dce11e2df1920a5b9b649
u1274265 <u1274265@utah.edu>
2019-11-30 15:26:15 -0700

    some changes

--- MagicRecall2/Source/MagicRecall2/ProjectileEye.cpp
+++ MagicRecall2/Source/MagicRecall2/ProjectileEye.cpp
@@ -1,7 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "CoreMinimal.h"
+#include "Engine.h"
 #include "ProjectileEye.h"
+#include "Components/StaticMeshComponent.h"
 
 // Sets default values
 AProjectileEye::AProjectileEye()
